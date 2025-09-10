bbfcf850f6eb9629e5247bf54d6675a5aebfeef3
Sahand-feizi <sahandfeizi87@gmail.com>
2025-09-10 11:03:22 +0330

    creating keenSlider component

---
+++ frontend/src/app/(blog)/_components/KeenSlider.jsx
@@ -0,0 +1,32 @@
+'use client'
+import React from "react"
+import { useKeenSlider } from "keen-slider/react"
+import BlogItem from "./BlogItem"
+import "keen-slider/keen-slider.min.css"
+
+function KeenSlider({ blogs, breakpoints, perView}) {
+    const [sliderRef] = useKeenSlider({
+        loop: false,
+        mode: "free",
+        
+        slides: {
+            perView: perView,
+            spacing: 15,
+        },
+        breakpoints: breakpoints
+    })
+
+    return (
+        <div ref={sliderRef} className="keen-slider">
+            {
+                blogs.map(item => (
+                    <div key={item._id} className="keen-slider__slide number-slide1 flex justify-center">
+                        <BlogItem className={'!h-[20rem]'} {...item} />
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
+
+export default KeenSlider
