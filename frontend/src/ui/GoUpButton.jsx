5736696982918d605975e5585cdd04708f84a8e1
Sahand-feizi <sahandfeizi87@gmail.com>
2025-09-17 09:24:20 +0330

    creating GoUpButton component

---
+++ frontend/src/ui/GoUpButton.jsx
@@ -0,0 +1,17 @@
+"use client"
+import React from 'react'
+import Button from './Button'
+import { RiArrowDropUpFill } from 'react-icons/ri'
+
+function GoUpButton() {
+    return (
+        <Button
+            variant={'white'}
+            onClick={() => window.scrollTo(0, 0)}
+            className={'w-[50px] h-[50px] !p-0 !text-[20px] rounded-full flex items-center justify-center'}>
+            <RiArrowDropUpFill className='!text-2xl' />
+        </Button>
+    )
+}
+
+export default GoUpButton
