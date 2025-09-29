27051c1fa84e801dcb233b3553b682ddfed33c04
Sahand-feizi <sahandfeizi87@gmail.com>
2025-09-29 15:27:23 +0330

    fix some ui bogs

--- frontend/src/app/(blog)/_components/comment/BlogComments.jsx
+++ frontend/src/app/(blog)/_components/comment/BlogComments.jsx
@@ -3,25 +3,44 @@ import { useState } from "react";
 import Comment from "./Comment";
 import Button from "@/ui/Button";
 import Modal from "@/ui/Modal";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/context/AuthContext";
 
 function BlogComments({ blog: { comments, _id: blogId } }) {
     const [isOpen, setIsOpen] = useState(false)
+    const [parent, setParent] = useState(null)
+    const { user } = useAuth()
+    const router = useRouter()
+
+    const addNewCommentHandler = (value) => {
+        if (!user) {
+            router.push('/signin')
+        }
+        setParent(value)
+        setIsOpen(true)
+    }
 
     return (
         <>
             <div className="mb-10">
                 <div className="mb-2 flex items-center justify-between">
                     <h2 className="text-2xl font-bold text-secondary-0">نظرات</h2>
-                    <Button onClick={() => setIsOpen(true)} variant={'white'} className={'font-normal'}>ثبت نظر جدید</Button>
+                    <Button
+                        onClick={() => addNewCommentHandler(null)}
+                        variant={'white'}
+                        className={'font-normal'}
+                    >
+                        ثبت نظر جدید
+                    </Button>
                 </div>
-<Modal
-    open={isOpen}
-    onClose={() => setIsOpen(false)}
-    title={'نظر'}
-    description="kzvhj"
->
+                <Modal
+                    title={parent ? "پاسخ به نظر" : "نظر جدید"}
+                    description={parent ? parent.user.name : "نظر خود را وارد کنید"}
+                    open={isOpen}
+                    onClose={() => setIsOpen(false)}
+                >
 
-</Modal>
+                </Modal>
                 <div className="space-y-8 post-comments bg-secondary-950 rounded-xl py-6 px-3 lg:px-6 ">
                     {comments.length > 0 ? (
                         comments.map((comment) => {
@@ -30,6 +49,7 @@ function BlogComments({ blog: { comments, _id: blogId } }) {
                                     <div className="border border-secondary-700 rounded-xl p-2 sm:p-4 mb-3">
                                         <Comment
                                             comment={comment}
+                                            addNewCommentHandler={addNewCommentHandler}
                                         />
                                     </div>
                                     <div className="post-comments__answer mr-2 sm:mr-8 space-y-3">
--- frontend/src/ui/Modal.jsx
+++ frontend/src/ui/Modal.jsx
@@ -15,7 +15,7 @@ function Modal({ open, onClose, title, children, description = "" }) {
         <div
           ref={ref}
           className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
-        rounded-lg bg-secondary-0 p-4 shadow-lg transition-all duration-500 ease-out
+        rounded-lg bg-secondary-950 p-4 shadow-lg transition-all duration-500 ease-out
         w-[calc(100vw-2rem)] md:max-w-lg max-h-[calc(100vh-2rem)] overflow-y-auto"
         >
           <div
@@ -23,7 +23,7 @@ function Modal({ open, onClose, title, children, description = "" }) {
           border-b-secondary-300 pb-2 mb-6"
           >
             <div>
-              <p className="text-secondary-700 font-bold text-base">{title}</p>
+              <p className="text-secondary-200 font-bold text-base">{title}</p>
               <p className="text-secondary-400 text-sm lg:text-base">
                 {description}
               </p>
