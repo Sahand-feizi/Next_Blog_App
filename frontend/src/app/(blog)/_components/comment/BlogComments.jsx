28958749affe618e43cac3732f13bbae86da7cd9
Sahand-feizi <sahandfeizi87@gmail.com>
2025-09-29 15:14:32 +0330

    creating Modal component

--- frontend/src/app/(blog)/_components/comment/BlogComments.jsx
+++ frontend/src/app/(blog)/_components/comment/BlogComments.jsx
@@ -1,15 +1,27 @@
+'use client'
+import { useState } from "react";
 import Comment from "./Comment";
 import Button from "@/ui/Button";
+import Modal from "@/ui/Modal";
 
 function BlogComments({ blog: { comments, _id: blogId } }) {
+    const [isOpen, setIsOpen] = useState(false)
 
     return (
         <>
             <div className="mb-10">
                 <div className="mb-2 flex items-center justify-between">
                     <h2 className="text-2xl font-bold text-secondary-0">نظرات</h2>
-                    <Button variant={'white'} className={'font-normal'}>ثبت نظر جدید</Button>
+                    <Button onClick={() => setIsOpen(true)} variant={'white'} className={'font-normal'}>ثبت نظر جدید</Button>
                 </div>
+<Modal
+    open={isOpen}
+    onClose={() => setIsOpen(false)}
+    title={'نظر'}
+    description="kzvhj"
+>
+
+</Modal>
                 <div className="space-y-8 post-comments bg-secondary-950 rounded-xl py-6 px-3 lg:px-6 ">
                     {comments.length > 0 ? (
                         comments.map((comment) => {
