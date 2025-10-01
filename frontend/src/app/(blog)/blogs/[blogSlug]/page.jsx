ea5773557d8b9230775bc45ef78bcbaccc483123
Sahand-feizi <sahandfeizi87@gmail.com>
2025-10-01 14:52:44 +0330

    creating generateMetadata for blog page

--- frontend/src/app/(blog)/blogs/[blogSlug]/page.jsx
+++ frontend/src/app/(blog)/blogs/[blogSlug]/page.jsx
@@ -1,6 +1,18 @@
 import React, { Suspense } from 'react'
 import BlogPage from 'app/(blog)/_components/BlogPage';
 import LoadingSpinner from '@/ui/LoadingSpinner';
+import { getBlogBySlugApi } from '@/services/blogServices';
+
+export async function generateMetadata({ params }) {
+  const { blogSlug } = params || {}
+
+  const blog = await getBlogBySlugApi(blogSlug)
+
+  return {
+    title: blog.title,
+    description: blog.briefText
+  }
+}
 
 async function page({ params }) {
   return (
