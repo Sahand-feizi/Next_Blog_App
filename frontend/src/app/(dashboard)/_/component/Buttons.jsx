aa1a4688aeb8a1af39826771463be1dcb268777d
Sahand-feizi <sahandfeizi87@gmail.com>
2025-10-02 10:14:49 +0330

    creating first section of the dashboard page

--- frontend/src/app/(dashboard)/_/component/Buttons.jsx
+++ frontend/src/app/(dashboard)/_/component/Buttons.jsx
@@ -1,7 +1,8 @@
 'use client'
 import { useAuth } from '@/context/AuthContext'
 import Button from '@/ui/Button'
-import { ArrowRightStartOnRectangleIcon } from '@heroicons/react/24/outline'
+import { ArrowRightStartOnRectangleIcon, PlusIcon } from '@heroicons/react/24/outline'
+import Link from 'next/link'
 import React from 'react'
 
 export function ExitButton() {
@@ -9,8 +10,21 @@ export function ExitButton() {
 
     return (
         <Button variant={'danger'} onClick={logout} className={'sidbar_btn'}>
-            <ArrowRightStartOnRectangleIcon className='w-6 h-6'/>
+            <ArrowRightStartOnRectangleIcon className='w-6 h-6' />
             <span className='hidden lg:flex'>خروج</span>
         </Button>
+    )
+}
+
+export function CreateBlog() {
+    return (
+        <Link
+            className='py-2 px-2 sm:px-3 rounded-full bg-secondary-0 text-secondary-800 
+            flex items-center gap-2 font-medium'
+            href={'/profile/blogs/create'}
+        >
+            <PlusIcon className='w-6 h-6' />
+            <span className='hidden sm:flex'>بلاگ جدید</span>
+        </Link>
     )
 }
--- frontend/src/app/(dashboard)/profile/page.jsx
+++ frontend/src/app/(dashboard)/profile/page.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { CreateBlog } from '../_/component/Buttons'
 
 function page() {
   return (
     <div>
-      
+      <div className='flex items-center justify-between'>
+        <div className='space-y-2'>
+          <h2 className='text-2xl md:text-3xl text-secondary-0 font-bold'>داشبورد</h2>
+          <p className='text-sm hidden sm:flex text-secondary-300 font-normal'>
+            تعداد بلاگ ها، کامنت ها و آنالیزی از اتفاقات
+          </p>
+        </div>
+        <CreateBlog />
+      </div>
     </div>
   )
 }
