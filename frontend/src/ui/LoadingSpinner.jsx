9572495f5924139fc456f48680130c1f52e28d47
Sahand-feizi <sahandfeizi87@gmail.com>
2025-11-05 16:04:45 +0330

    fix deploy bugs

--- frontend/src/ui/LoadingSpinner.jsx
+++ frontend/src/ui/LoadingSpinner.jsx
@@ -1,19 +1,9 @@
 "use client"
 import React from 'react'
-import { ThreeDots } from 'react-loader-spinner'
 
 function LoadingSpinner({ width = '80', height='80', color = '#fff' }) {
     return (
-        <ThreeDots
-            visible={true}
-            height={height}
-            width={width}
-            color={color}
-            radius="9"
-            ariaLabel="three-dots-loading"
-            wrapperStyle={{}}
-            wrapperClass="m-auto"
-        />
+        <div>Loading...</div>
     )
 }
 
