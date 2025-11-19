254842e64f0414b18d14c93753876219dc2f3782
Sahand-feizi <sahandfeizi87@gmail.com>
2025-11-19 16:33:14 +0330

    fix bug

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -1,25 +1,25 @@
-import { NextResponse } from "next/server";
-import middlewareAuth from "./utils/middlewareAuth";
+// import { NextResponse } from "next/server";
+// import middlewareAuth from "./utils/middlewareAuth";
 
-export async function middleware(req) {
-    const user = await middlewareAuth(req.cookies);
-    const { pathname } = req.nextUrl;
+// export async function middleware(req) {
+//     const user = await middlewareAuth(req.cookies);
+//     const { pathname } = req.nextUrl;
 
-    if (pathname.startsWith('/profile')) {
-        if (!user) {
-            return NextResponse.redirect(new URL('/signin', req.url));
-        }
-    }
+//     if (pathname.startsWith('/profile')) {
+//         if (!user) {
+//             return NextResponse.redirect(new URL('/signin', req.url));
+//         }
+//     }
 
-    if (pathname.startsWith('/signin') || pathname.startsWith('/signup')) {
-        if (user) {
-            return NextResponse.redirect(new URL('/', req.url));
-        }
-    }
+//     if (pathname.startsWith('/signin') || pathname.startsWith('/signup')) {
+//         if (user) {
+//             return NextResponse.redirect(new URL('/', req.url));
+//         }
+//     }
 
-    return NextResponse.next();
-}
+//     return NextResponse.next();
+// }
 
-export const config = {
-    matcher: ['/profile', '/signup', '/signin']
-};
+// export const config = {
+//     matcher: ['/profile', '/signup', '/signin']
+// };
